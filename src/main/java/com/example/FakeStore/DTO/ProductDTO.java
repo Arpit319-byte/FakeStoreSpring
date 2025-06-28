4df68c496b84e5e8d6427b3faa63112940271a3f
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2025-06-28 16:15:07 +0530

    Added the Attributes for the ProductDTO

--- src/main/java/com/example/FakeStore/DTO/ProductDTO.java
+++ src/main/java/com/example/FakeStore/DTO/ProductDTO.java
@@ -1,4 +1,17 @@
 package com.example.FakeStore.DTO;
 
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Component
 public class ProductDTO {
+
+    String productName;
+    String productCategory;
+
 }
