b13be51cfc874f1d1f0b1af5cef402c25edabe7f
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2025-06-28 15:50:53 +0530

    Added the getAllCategory method

--- src/main/java/com/example/FakeStore/Service/FakeStoreCategoryService.java
+++ src/main/java/com/example/FakeStore/Service/FakeStoreCategoryService.java
@@ -1,4 +1,13 @@
 package com.example.FakeStore.Service;
 
-public class FakeStoreCategoryService {
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class FakeStoreCategoryService implements ICategoryService {
+
+    @Override
+    public String getAllCategory(List<String> productByCategory) {
+        return "";
+    }
 }
