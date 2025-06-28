d20e3020045d7da929d440236555bb4a72facf71
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2025-06-28 16:06:30 +0530

    Added the implementation of the getAllCategories

--- src/main/java/com/example/FakeStore/Controller/CategoryController.java
+++ src/main/java/com/example/FakeStore/Controller/CategoryController.java
@@ -1,15 +1,25 @@
 package com.example.FakeStore.Controller;
 
+import com.example.FakeStore.Service.ICategoryService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping
+@RequestMapping("api/v1/Product/Category")
 public class CategoryController {
 
+    private final ICategoryService iCategoryService;
+
+    public CategoryController(ICategoryService _iCategoryService){
+        this.iCategoryService=_iCategoryService;
+    }
+
     @GetMapping
-    public String getAllCategories(){
-        return "";
+    public String getAllCategories(List<String> productByCategory){
+        iCategoryService.getAllCategory(productByCategory);
+        return "O.K.get";
     }
 }
