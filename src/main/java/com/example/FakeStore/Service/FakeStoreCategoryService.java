596d3cfb6ee98b8053af2e2338a835642e7a799e
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2025-06-29 23:07:08 +0530

    Calling GatewaySerivce from Serivce

--- src/main/java/com/example/FakeStore/Service/FakeStoreCategoryService.java
+++ src/main/java/com/example/FakeStore/Service/FakeStoreCategoryService.java
@@ -1,16 +1,24 @@
 package com.example.FakeStore.Service;
 
 import com.example.FakeStore.DTO.CategoryDTO;
+import com.example.FakeStore.Gateway.FakeStoreCategoryGateway;
+import com.example.FakeStore.Gateway.ICategoryGateway;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class FakeStoreCategoryService implements ICategoryService {
 
+    private final ICategoryGateway iCategoryGateway;
+
+    public FakeStoreCategoryService(ICategoryGateway _iCategoryGateway){
+        this.iCategoryGateway=_iCategoryGateway;
+    }
     @Override
-    public List<CategoryDTO> getAllCategory() {
-        return new ArrayList<>();
+    public List<CategoryDTO> getAllCategory() throws IOException {
+        return iCategoryGateway.getAllCategories();
     }
 }
--- src/main/java/com/example/FakeStore/Service/ICategoryService.java
+++ src/main/java/com/example/FakeStore/Service/ICategoryService.java
@@ -2,9 +2,10 @@ package com.example.FakeStore.Service;
 
 import com.example.FakeStore.DTO.CategoryDTO;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface ICategoryService {
 
-    public List<CategoryDTO> getAllCategory();
+    public List<CategoryDTO> getAllCategory() throws IOException;
 }
