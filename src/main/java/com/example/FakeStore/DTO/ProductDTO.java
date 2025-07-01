33787e4456d925cf96fc40bb3df87a041a0b67d6
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2025-07-01 19:29:13 +0530

    Added the attributes for the ProductDTO

--- src/main/java/com/example/FakeStore/DTO/ProductDTO.java
+++ src/main/java/com/example/FakeStore/DTO/ProductDTO.java
@@ -1,4 +1,15 @@
 package com.example.FakeStore.DTO;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ProductDTO {
+
+    String title;
+    String price;
+    String description;
 }
