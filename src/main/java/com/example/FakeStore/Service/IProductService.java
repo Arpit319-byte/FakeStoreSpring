66ab49c7eb03d20cbfbc5fabb39d551fd17d79f5
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2025-07-01 19:56:12 +0530

    Added method for fetching all products

--- src/main/java/com/example/FakeStore/Service/IProductService.java
+++ src/main/java/com/example/FakeStore/Service/IProductService.java
@@ -1,4 +1,9 @@
 package com.example.FakeStore.Service;
 
+import com.example.FakeStore.DTO.ProductDTO;
+import java.util.List;
+
 public interface IProductService {
+
+    public List<ProductDTO>  getAllProduct();
 }
