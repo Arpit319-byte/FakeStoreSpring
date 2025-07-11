9adf2e167e8eca329fa1e7daa91146621ada7fbc
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2025-07-11 22:29:45 +0530

    PoductService implements the IproductService

--- src/main/java/com/example/FakeStore/Service/ProductService.java
+++ src/main/java/com/example/FakeStore/Service/ProductService.java
@@ -1,4 +1,37 @@
 package com.example.FakeStore.Service;
 
-public class ProductService {
+import com.example.FakeStore.DTO.ProductDTO;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class ProductService implements IProductService {
+    // This is a placeholder service implementation for ProductService.
+
+    @Override
+    public List<ProductDTO> getAllProduct() throws IOException {
+        return List.of();
+    }
+
+    @Override
+    public ProductDTO getProductById(long id) throws IOException {
+        return null;
+    }
+
+    @Override
+    public List<ProductDTO> getProductByCategory() throws IOException {
+        return List.of();
+    }
+
+    @Override
+    public ProductDTO createProduct(ProductDTO productDTO) throws IOException {
+        return null;
+    }
+
+    @Override
+    public boolean deleteProductById(Long id) throws IOException {
+        return false;
+    }
 }
