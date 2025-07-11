8c45daa858de905ca03d2978e07e1c53cb3b2b4d
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2025-07-11 18:18:32 +0530

    Added the attributes for the BaseModel Class

--- src/main/java/com/example/FakeStore/model/BaseModel.java
+++ src/main/java/com/example/FakeStore/model/BaseModel.java
@@ -1,4 +1,41 @@
 package com.example.FakeStore.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.Instant;
+import java.util.Date;
+
+@MappedSuperclass
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BaseModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected  long id;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    protected Instant createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    protected Instant updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = Instant.now();
+        this.updatedAt = Instant.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = Instant.now();
+    }
 }
