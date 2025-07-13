a29a4f631fac369b0d409bf6952a5fc987111c15
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2025-07-13 16:58:12 +0530

    Added the attributes for Address Entity

--- src/main/java/com/example/FakeStore/model/Address.java
+++ src/main/java/com/example/FakeStore/model/Address.java
@@ -1,4 +1,22 @@
 package com.example.FakeStore.model;
 
-public class Address  {
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Address extends BaseEntity {
+
+    private String city;
+    private String street;
+    private String number;
+    private String zipcode;
+
+    private GeoLocation geolocation;
+
+
 }
