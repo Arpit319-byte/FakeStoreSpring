639a4dad34cb5e54ce70d5a69f39cf7cf9e1933f
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2025-07-13 16:49:17 +0530

    Added the attributes for User Entity

--- src/main/java/com/example/FakeStore/model/User.java
+++ src/main/java/com/example/FakeStore/model/User.java
@@ -1,4 +1,26 @@
 package com.example.FakeStore.model;
 
-public class User {
+import com.example.FakeStore.DTO.Geolocation;
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User extends BaseEntity {
+
+    private String email;
+    private String password;
+    private String username;
+    private String phone;
+    private String firstName;
+    private String lastName;
+
+    private Address address;
+    private GeoLocation geolocation;
+
+
 }
